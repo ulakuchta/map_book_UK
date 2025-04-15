4509c4fdf7dfc629ec03d8e68e0a6110f9fd4b33
ulakuchta <ulakuchta@op.pl>
2025-04-15 19:11:14 +0200

    wip: update user functionality

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from utils.model import users
-from utils.controler import get_user_info, add_user, remove_user
+from utils.controler import get_user_info, add_user, remove_user, update_user
 
 
 def main():
@@ -8,6 +8,7 @@ def main():
     print('1 - wyświetl co u znajomych')
     print('2 - dodaj znajomego')
     print('3 - usuń znajomego')
+    print('4 - zaktualizuj dane znajomego')
     print('============================')
     while True:
         choice: str = input('Wybierz opcję MENU: ')
@@ -15,6 +16,7 @@ def main():
         if choice == '1': get_user_info(users)
         if choice == '2': add_user(users)
         if choice == '3': remove_user(users)
+        if choice == '4': update_user(users)
 
 
 if __name__ == '__main__':
--- utils/controler.py
+++ utils/controler.py
@@ -15,3 +15,12 @@ def remove_user(users_data: list) -> None:
     for user in users_data:
         if user['name'] == user_name:
             users_data.remove(user)
+
+
+def update_user(users_data: list) -> None:
+    user_name: str = input('Podaj imię użytkownika do aktualizacji: ')
+    for user in users_data:
+        if user['name'] == user_name:
+            user['name'] = input('Podaj nowe imię użytkownika: ')
+            user['location'] = input('Podaj nową lokalizacje użytkownika: ')
+            user['posts'] = input('Podaj liczbę postów: ')
