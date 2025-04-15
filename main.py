f6430bf1a8511a17a469f1c127d70f6f9c92aa6a
ulakuchta <ulakuchta@op.pl>
2025-04-15 18:30:31 +0200

    wip: add new user functionality

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from utils.model import users
-from utils.controler import get_user_info
+from utils.controler import get_user_info, add_user
 
 
 def main():
     print('=============MENU===========')
     print('0 - zakończ program')
     print('1 - wyświetl co u znajomych')
+    print('2 - dodaj znajomego')
     print('============================')
     while True:
         choice: str = input('Wybierz opcję MENU: ')
         if choice == '0': break
         if choice == '1': get_user_info(users)
+        if choice == '2': add_user(users)
 
 
 if __name__ == '__main__':
--- utils/controler.py
+++ utils/controler.py
@@ -1,3 +1,10 @@
 def get_user_info(users_data: list) -> None:
     for user in users_data:
         print(f'Twój znajomy {user["name"]} z miejscowości {user["location"]} opublikował {user["posts"]} postów.')
+
+
+def add_user(users_data: list) -> None:
+    new_name: str = input('Podaj imię nowego znajomego: ')
+    new_location: str = input('Podaj lokalizację: ')
+    new_posts: str = input('Podaj liczbę postów nowego znajomego: ')
+    users_data.append({'name': new_name, 'location': new_location, 'posts': new_posts}, )
