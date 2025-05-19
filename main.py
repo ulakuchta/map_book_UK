a1fbe49b4c880c28ace43a5dd052d0025bf2cb52
ulakuchta <ulakuchta@op.pl>
2025-05-19 09:38:03 +0200

    wip: tk gui frontend

--- main.py
+++ main.py
@@ -1,25 +1,82 @@
-from utils.model import users
-from utils.controler import get_user_info, add_user, remove_user, update_user, get_map
+from tkinter import *
+
+import tkintermapview
+
+root = Tk()
+root.geometry("1200x760")
+root.title("Map Book UK")
 
 
-def main():
-    print('=============MENU===========')
-    print('0 - zakończ program')
-    print('1 - wyświetl co u znajomych')
-    print('2 - dodaj znajomego')
-    print('3 - usuń znajomego')
-    print('4 - zaktualizuj dane znajomego')
-    print('5 - Wygeneruj mapę znajomych')
-    print('============================')
-    while True:
-        choice: str = input('Wybierz opcję MENU: ')
-        if choice == '0': break
-        if choice == '1': get_user_info(users)
-        if choice == '2': add_user(users)
-        if choice == '3': remove_user(users)
-        if choice == '4': update_user(users)
-        if choice == '5': get_map(users)
+ramka_lista_obiektow=Frame(root)
+ramka_formularz=Frame(root)
+ramka_szczegoly_obiektow=Frame(root)
+ramka_mapa=Frame(root)
 
+ramka_lista_obiektow.grid(row=0, column=0)
+ramka_formularz.grid(row=0, column=1)
+ramka_szczegoly_obiektow.grid(row=1, column=0,columnspan=2)
+ramka_mapa.grid(row=2, column=0,columnspan=2)
 
-if __name__ == '__main__':
-    main()
+# ramka_lista_obiektow
+label_lista_obiektow=Label(ramka_lista_obiektow, text="Lista użytkowników")
+label_lista_obiektow.grid(row=0, column=0, columnspan=3)
+listbox_lista_obiektow=Listbox(ramka_lista_obiektow, width=50, height=10)
+listbox_lista_obiektow.grid(row=1, column=0, columnspan=3)
+button_pokaz_szczegoly_obiektu=Button(ramka_lista_obiektow, text='Pokaż szczegóły')
+button_pokaz_szczegoly_obiektu.grid(row=2, column=0)
+button_usun_obiekt=Button(ramka_lista_obiektow, text='Usuń użytkownika')
+button_usun_obiekt.grid(row=2, column=1)
+button_edytuj_obiekt=Button(ramka_lista_obiektow, text='Edytuj obiekt')
+button_edytuj_obiekt.grid(row=2, column=2)
+
+# ramka_formularz
+label_formularz=Label(ramka_formularz, text="Formularz")
+label_formularz.grid(row=0, column=0,columnspan=2)
+label_name=Label(ramka_formularz, text="Imię:")
+label_name.grid(row=1, column=0, sticky=W)
+label_surname=Label(ramka_formularz, text="Nazwisko:")
+label_surname.grid(row=2, column=0, sticky=W)
+label_location=Label(ramka_formularz, text="Miejscowość:")
+label_location.grid(row=3, column=0, sticky=W)
+label_posts=Label(ramka_formularz, text="Postów:")
+label_posts.grid(row=4, column=0, sticky=W)
+
+entry_name=Entry(ramka_formularz)
+entry_name.grid(row=1, column=1)
+entry_surname=Entry(ramka_formularz)
+entry_surname.grid(row=2, column=1)
+entry_location=Entry(ramka_formularz)
+entry_location.grid(row=3, column=1)
+entry_posts=Entry(ramka_formularz)
+entry_posts.grid(row=4, column=1)
+
+button_dodaj_obiekt=Button(ramka_formularz, text='Dodaj obiekt')
+button_dodaj_obiekt.grid(row=5, column=0, columnspan=2)
+
+# ramka_szczegoly_obiektow
+label_szczegoly_obiektow=Label(ramka_szczegoly_obiektow, text="Szczegóły obiektu")
+label_szczegoly_obiektow.grid(row=0, column=0)
+label_szczegoly_name=Label(ramka_szczegoly_obiektow, text="Imię:")
+label_szczegoly_name.grid(row=1, column=0)
+label_szczegoly_name_wartosc=Label(ramka_szczegoly_obiektow, text="....")
+label_szczegoly_name_wartosc.grid(row=1, column=1)
+label_szczegoly_surname=Label(ramka_szczegoly_obiektow, text="Nazwisko:")
+label_szczegoly_surname.grid(row=1, column=2)
+label_szczegoly_surname_wartosc=Label(ramka_szczegoly_obiektow, text="....")
+label_szczegoly_surname_wartosc.grid(row=1, column=3)
+label_szczegoly_location=Label(ramka_szczegoly_obiektow, text="Miejscowość:")
+label_szczegoly_location.grid(row=1, column=4)
+label_szczegoly_location_wartosc=Label(ramka_szczegoly_obiektow, text="....")
+label_szczegoly_location_wartosc.grid(row=1, column=5)
+label_szczegoly_posts=Label(ramka_szczegoly_obiektow, text="Posty:")
+label_szczegoly_posts.grid(row=1, column=6)
+label_szczegoly_posts_wartosc=Label(ramka_szczegoly_obiektow, text="....")
+label_szczegoly_posts_wartosc.grid(row=1, column=7)
+
+# ramka_mapa
+map_widget = tkintermapview.TkinterMapView(ramka_mapa, width=1200, height=500, corner_radius=5)
+map_widget.grid(row=0, column=0, columnspan=2)
+map_widget.set_position(52.23,21.0)
+map_widget.set_zoom(6)
+
+root.mainloop()
--- utils/model.py
+++
@@ -1,6 +0,0 @@
-users: list = [
-    {'name': 'Maciej', 'location': 'Łódź', 'posts': 100},
-    {'name': 'Mateusz', 'location': 'Poznań', 'posts': 200},
-    {'name': 'Maciej01', 'location': 'Siedlce', 'posts': 300},
-    {'name': 'Konrad', 'location': 'Grudziądz', 'posts': 400},
-]
